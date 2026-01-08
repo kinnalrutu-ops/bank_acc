23895595b18d439f3b04b92cda39b67bf8085169
Rutu <kinnalrutu@gmail.com>
2026-01-08 15:14:33 +0530

    Fix get_details_info function parameters and output

--- bank_acc.py
+++ bank_acc.py
@@ -3,11 +3,10 @@ import sys
 def get_details_info(acc_no, holder_name,type,balance):
   
     return (
-        f"acc_no:{ACC_NO},"
-        f"holder_name:{HOLDER_NAME},"
-        f"Department:{department},"
-        f"Salary:{salary:.2f}"
+        f"acc_no:{acc_no},"
+        f"holder_name:{holder_name},"
+        f"type:{type},"
+        f"balance:{balance}"
     )
 if __name__ == "__main__":
-    print("=== Employee details ===")
-    print(get_employee_info("John Doe","E101","IT",55000))
+    print(get_employee_info("012345678","abc","savings",55000))
