bb558638c35b68cacc3d3a2cfb1ec5aded80d4e9
Rutu <kinnalrutu@gmail.com>
2026-01-08 15:20:29 +0530

    Update test_det.py

--- test_det.py
+++ test_det.py
@@ -4,15 +4,15 @@ from bank_acc import get_details_info
 def test_details():
    
     acc_no = "012345678"
-    holder_name = "E202"
-     type = "HR"
-    balance = 500000
+    holder_name = "abc"
+     type = "savings"
+    balance = 55000
     
     expected_output = (
         "Account No:012345678,"
         "holder_name:abc,"
         "type:savings,"
-        "balance:550000.00"
+        "balance:55000"
     )
 
     assert get_bank_info(acc_no, holder_name, type, balance) == expected_output
